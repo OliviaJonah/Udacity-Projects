46b45f000fecc0ed353afaee5e72dd50f0332789
Olivia Jonah <oj17us@gmail.com>
2017-05-24 19:31:54 -0500

    Add files via upload

---
+++ cleaning_.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 24 19:27:08 2017
+
+@author: Victor
+"""
+
+from pandas import DataFrame, read_csv, cut
+import numpy as np
+
+df = read_csv('baseball_data.csv')
+df = df[(df.avg > 0.0) & (df.HR > 0)]
+
+df['avg_category'] = cut(df.avg, 
+						bins = np.linspace(0.1, 0.35, 6), 
+						right=False)
+
+avg_handedness_count_df = df.drop(['height', 'weight', 'avg'], axis=1).groupby(by=['avg_category', 'handedness']).sum().reset_index()
+avg_partical_count  = df.drop(['height', 'weight', 'name', 'avg', 'handedness'], axis=1).groupby(by=['avg_category']).sum()
+avg_count_df = avg_partical_count.add_prefix('total_').reset_index()
+final_df = avg_handedness_count_df.merge(avg_count_df, on='avg_category')
+final_df['HR_Percent'] = final_df.HR / final_df.total_HR * 100.
+
+mean_hr_group = df.drop(['height', 'weight', 'avg'], axis=1).groupby(by=['avg_category', 'handedness']).mean().add_prefix('mean_').reset_index()
+final_df = final_df.merge(mean_hr_group, on=['avg_category', 'handedness'])
+final_df.to_csv('cleaned_baseball.csv')
